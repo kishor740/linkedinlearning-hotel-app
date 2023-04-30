35a8e353998168d25ca96114c726f7af4925e9a4
kishor adhikari <kishoradhikari14@gmail.com>
2023-04-29 21:55:03 -0500

    Added under development notice

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         App setup
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>This site is under development. Please check back later!</p>
       </header>
     </div>
   );
