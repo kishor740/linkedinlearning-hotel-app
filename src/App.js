83a69bc0397a8333d3ae7fe936b43d362afc5c07
kishor adhikari <kishoradhikari14@gmail.com>
2023-04-29 21:12:47 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Learn react js
+         App setup
         </p>
         <a
           className="App-link"
